b41652bba0a90fd4e860fb7d167518e370ad7cc1
Lisa Durdun <lisa.durdun@gmail.com>
2024-01-11 15:05:41 +0100

    add new eventlisteners for clicking on arrow keys, add favicon

--- js/trash.js
+++ js/trash.js
@@ -1,77 +1,77 @@
 const trash = [
   {
     name: "pizza box",
-    img: "/the-trash-separator/images/pizza-box.png",
+    img: "./images/pizza-box.png",
     bin: "black",
   },
   {
     name: "milk carton",
-    img: "/the-trash-separator/images/milk-carton.png",
+    img: "./images/milk-carton.png",
     bin: "yellow",
   },
   {
     name: "baking paper",
-    img: "/the-trash-separator/images/baking-paper.png",
+    img: "./images/baking-paper.png",
     bin: "black",
   },
   {
     name: "egg carton",
-    img: "/the-trash-separator/images/egg-carton.png",
+    img: "./images/egg-carton.png",
     bin: "blue",
   },
   {
     name: "used tissue",
-    img: "/the-trash-separator/images/tissues.png",
+    img: "./images/tissues.png",
     bin: "black",
   },
   {
     name: "coffee filter",
-    img: "/the-trash-separator/images/coffee-filter.png",
+    img: "./images/coffee-filter.png",
     bin: "green",
   },
   {
     name: "newspaper",
-    img: "/the-trash-separator/images/newspaper.png",
+    img: "./images/newspaper.png",
     bin: "blue",
   },
   {
     name: "coffee cup",
-    img: "/the-trash-separator/images/coffee-cup.png",
+    img: "./images/coffee-cup.png",
     bin: "yellow",
   },
   {
     name: "lightbulb",
-    img: "/the-trash-separator/images/light-bulb.png",
+    img: "./images/light-bulb.png",
     bin: "black",
   },
   {
     name: "tin can",
-    img: "/the-trash-separator/images/tin-can.png",
+    img: "./images/tin-can.png",
     bin: "yellow",
   },
   {
     name: "deodorant",
-    img: "/the-trash-separator/images/deodorant.png",
+    img: "./images/deodorant.png",
     bin: "yellow",
   },
   {
     name: "toothbrush",
-    img: "/the-trash-separator/images/toothbrush.png",
+    img: "./images/toothbrush.png",
     bin: "black",
   },
   {
     name: "razor",
-    img: "/the-trash-separator/images/razor.png",
+    img: "./images/razor.png",
     bin: "black",
   },
   {
     name: "flower buquet",
-    img: "/the-trash-separator/images/flowers.png",
+    img: "./images/flowers.png",
     bin: "green",
   },
   {
     name: "eggshell",
-    img: "/the-trash-separator/images/egg-shells.png",
+    img: "./images/egg-shells.png",
     bin: "green",
   },
 ];
